9b56d54a6c9b2e85b423b6eebcad647a7ed34664
DUO <zhaoduozd@outlook.com>
2018-02-25 13:16:50 +0800

    initial job

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+
+var url = 'mongodb://localhost:27017/sweetyfitnode';
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
+});
+
+
+
 
 app.get('/', function(req, res) {
-    res.sendFile('/Users/duozhao/Documents/FinalYearProject/FitNode/test.gif');
+    res.json({user: 'tobi'});
+
 })
 
 app.listen(3000);
